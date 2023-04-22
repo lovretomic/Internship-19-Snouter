40001c19adc16e535d512135b80940b171c5d60e
lovretomic <tomiclovre05@gmail.com>
2023-04-22 23:02:44 +0200

    added app services

---
+++ Snouter/Snouter.Application/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Snouter.Application.Repositories;
+
+namespace Snouter.Application;
+
+public static class ApplicationServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddSingleton<IUserRepository, UserRepository>();
+        return services;
+    }
+}
