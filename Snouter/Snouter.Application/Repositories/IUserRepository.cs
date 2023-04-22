993b9a3b9d41e9b934d69769e654e77a3471d5c8
lovretomic <tomiclovre05@gmail.com>
2023-04-22 22:56:24 +0200

    added User repository

---
+++ Snouter/Snouter.Application/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Snouter.Application.Models.User;
+
+namespace Snouter.Application.Repositories;
+
+public interface IUserRepository
+{
+    Task<bool> CreateAsync(User user);
+    Task<User?> GetByIdAsync(Guid id);
+    Task<bool> UpdateAsync(User user);
+    Task<bool> DeleteByIdAsync(Guid id);
+}
---
+++ Snouter/Snouter.Application/Repositories/UserRepository.cs
@@ -0,0 +1,55 @@
+﻿using Snouter.Application.Models.User;
+
+namespace Snouter.Application.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private List<User> _users = new();
+    
+    public Task<bool> CreateAsync(User user)
+    {
+        if (_users.Contains(user))
+        {
+            return Task.FromResult(false);
+        }
+        _users.Add(user);
+        return Task.FromResult(true);
+    }
+
+    public Task<User?> GetByIdAsync(Guid id)
+    {
+        var user = _users.SingleOrDefault(x => x.Id == id);
+        return Task.FromResult(user);
+    }
+
+    public Task<bool> UpdateAsync(User user)
+    {
+        var existingUser = _users.SingleOrDefault(x => x.Id == user.Id);
+
+        if (existingUser is null)
+        {
+            return Task.FromResult(false);
+        }
+
+        existingUser.FullName = user.FullName;
+        existingUser.Email = user.Email;
+        existingUser.Password = user.Password;
+        existingUser.Location = user.Location;
+        existingUser.ProfilePicUrl = user.ProfilePicUrl;
+
+        return Task.FromResult(true);
+    }
+
+    public Task<bool> DeleteByIdAsync(Guid id)
+    {
+        var existingUser = _users.SingleOrDefault(x => x.Id == id);
+
+        if (existingUser is null)
+        {
+            return Task.FromResult(false);
+        }
+
+        _users.Remove(existingUser);
+        return Task.FromResult(true);
+    }
+}
