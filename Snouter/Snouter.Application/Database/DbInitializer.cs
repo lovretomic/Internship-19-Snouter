f688d0b86c5dd9f27f391d88400daafbf5803f58
lovretomic <tomiclovre05@gmail.com>
2023-04-26 23:14:26 +0200

    added initial DbInitializer functions

--- Snouter/Snouter.Application/Database/DbInitializer.cs
+++ Snouter/Snouter.Application/Database/DbInitializer.cs
@@ -10,6 +10,51 @@ public class DbInitializer
     {
         _dbConnectionFactory = dbConnectionFactory;
     }
-    
-    // Initializer functions
+
+    public async Task InitializeAsync()
+    {
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+
+        await connection.ExecuteAsync(@"
+            create table if not exists Users (
+                Id UUID primary key,
+                FullName TEXT,
+                Email TEXT,
+                Password TEXT,
+                CreatedAt date not null,
+                ProfilePicUrl TEXT,
+                Lat INTEGER,
+                Long INTEGER
+            );
+        ");
+
+        await connection.ExecuteAsync(@"
+            create table if not exists Items (
+                Id UUID,
+                AuthorId UUID,
+                Title TEXT,
+                CreatedAt date,
+                Subcategory TEXT,
+                Description TEXT,
+                ImageLinks TEXT[],
+                Price NUMERIC(15, 2),
+                Currency TEXT,
+                AdditionalProps JSON
+            );
+        ");
+
+        await connection.ExecuteAsync(@"
+            create table if not exists Categories (
+                Name TEXT
+            );
+        ");
+
+        await connection.ExecuteAsync(@"
+            create table if not exists Subcategories (
+                Name TEXT,
+                CategoryName TEXT,
+                AdditionalProps JSON
+            );
+        ");
+    }
 }
