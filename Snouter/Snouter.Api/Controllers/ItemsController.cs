44c33d24cec638403660428ef97dba55e87f4e1e
lovretomic <tomiclovre05@gmail.com>
2023-05-02 18:11:10 +0200

    updated validator for items

--- Snouter/Snouter.Api/Controllers/ItemsController.cs
+++ Snouter/Snouter.Api/Controllers/ItemsController.cs
@@ -2,6 +2,7 @@
 using Snouter.Api.Mapping;
 using Snouter.Application.Models.Item;
 using Snouter.Application.Repositories;
+using Snouter.Application.Services;
 using Snouter.Contracts.Requests;
 
 namespace Snouter.Api.Controllers;
@@ -9,10 +10,12 @@ namespace Snouter.Api.Controllers;
 public class ItemsController : ControllerBase
 {
     private readonly IItemRepository _itemRepository;
+    private readonly IItemService _itemService;
 
-    public ItemsController(IItemRepository itemRepository)
+    public ItemsController(IItemRepository itemRepository, IItemService itemService)
     {
         _itemRepository = itemRepository;
+        _itemService = itemService;
     }
 
     [HttpPost]
@@ -20,7 +23,7 @@ public class ItemsController : ControllerBase
     public async Task<IActionResult> Create([FromBody] CreateItemRequest request)
     {
         var item = request.MapToItem();
-        var isCreated = _itemRepository.CreateAsync(item).Result;
+        var isCreated = _itemService.CreateAsync(item).Result;
         if (!isCreated)
         {
             return BadRequest();
@@ -34,7 +37,7 @@ public class ItemsController : ControllerBase
     [Route(ApiEndpoints.Item.GetById)]
     public async Task<IActionResult> GetById([FromRoute] Guid id)
     {
-        var item = await _itemRepository.GetByIdAsync(id);
+        var item = await _itemService.GetByIdAsync(id);
         if (item is null)
         {
             return NotFound();
@@ -48,7 +51,7 @@ public class ItemsController : ControllerBase
     [Route(ApiEndpoints.Item.DeleteById)]
     public async Task<IActionResult> DeleteById([FromRoute] Guid id)
     {
-        var isDeleted = await _itemRepository.DeleteByIdAsync(id);
+        var isDeleted = await _itemService.DeleteByIdAsync(id);
         if (!isDeleted)
         {
             return BadRequest();
@@ -61,7 +64,7 @@ public class ItemsController : ControllerBase
     public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateItemRequest request)
     {
         var task = request.MapToTask(id);
-        var isUpdated = await _itemRepository.UpdateAsync(task);
+        var isUpdated = await _itemService.UpdateAsync(task);
         if (!isUpdated)
         {
             return NotFound();
